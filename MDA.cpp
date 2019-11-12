249d8ad0f15feefd91d81bd97ee1f0333e1374f2
Gauri18 <45160142+Gauri18@users.noreply.github.com>
2019-11-12 15:09:07 +0530

    Add files via upload

---
+++ MDA.cpp
@@ -0,0 +1,28 @@
+#include<iostream>
+using namespace std;
+int main()
+{
+int fa,la,i,j;
+cout<<"Enter rows:\n";
+cin>>fa;
+cout<<"Enter columns:\n";
+cin>>la;
+int arr[fa][la];
+cout<<"Enter row and column values:\n";
+for(i=0;i<fa;i++)
+{
+	for(j=0;j<la;j++)
+	{
+		
+		cin>>arr[i][j];
+	}
+}
+cout<<"Array:\n";
+for(i=0;i<fa;i++)
+{
+	for(j=0;j<la;j++)
+	{
+		cout<<arr[i][j];
+	}
+}
+	}
